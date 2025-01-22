97612ef7685a14452fb28915892e12b70a07f274
Enes SIRIN <enessirinbusiness@gmail.com>
2025-01-22 21:40:26 +0300

    categories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React from "react";
 import Header from "./components/header/Header";
+import Categories from "./components/categories/Categories";
+import Products from "./components/products/Products";
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Header />
-    </div>
+      <div className="home px-6 flex justify-between gap-10">
+         <div className="categories flex-1 overflow-auto max-h-[calc(100vh-112px)]">
+                 <Categories />
+         </div> 
+         <div className="products flex-[8]">
+             <div>
+                <Products />
+             </div>
+         </div>
+         <div>
+             <div>
+                 cart totals
+             </div>
+         </div>
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,35 @@
+function Categories() {
+  return (
+    <ul className="flex gap-4 flex-col text-center text-lg pb-20">
+      <li className="bg-green-700 px-6 py-10 text-white hover:cursor-pointer hover:bg-pink-700 transition-all">
+        <span>Tümü</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white hover:cursor-pointer hover:bg-pink-700 transition-all">
+        <span>Yiyecek</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white hover:cursor-pointer hover:bg-pink-700 transition-all">
+        <span>İçecek</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white hover:cursor-pointer hover:bg-pink-700 transition-all">
+        <span>İçecek</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white hover:cursor-pointer hover:bg-pink-700 transition-all">
+        <span>İçecek</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white hover:cursor-pointer hover:bg-pink-700 transition-all">
+        <span>İçecek</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white hover:cursor-pointer hover:bg-pink-700 transition-all">
+        <span>İçecek</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white hover:cursor-pointer hover:bg-pink-700 transition-all">
+        <span>İçecek</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white hover:cursor-pointer hover:bg-pink-700 transition-all">
+        <span>İçecek</span>
+      </li>
+    </ul>
+  )
+}
+
+export default Categories
